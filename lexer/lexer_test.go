f1e70b6f424a58671c848ab433212b783a9786f2
ceciliamvrie <cecilia.mvrie@gmail.com>
2020-05-19 00:12:35 -0500

    adds tests for more lexing identifiers and keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestNextToken(t *testing.T) {
+func TestOperatorsAndDelimiters(t *testing.T) {
 	input := `=+(){},;`
 
 	tests := []struct {
@@ -20,6 +20,56 @@ func TestNextToken(t *testing.T) {
 		{token.LBRACE, "{"},
 		{token.RBRACE, "}"},
 		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := lexer.NewInput(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - wrong token type. expected: %q, got: %q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - wrong literal. expected: %q, got: %q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestKeywordsAndIdentifiers(t *testing.T) {
+	input := `
+		let five = 5;
+
+		let add = func(x, y) {
+			x + y;
+		};
+	`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, 5},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNC, "func"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.RBRACE, "}"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
